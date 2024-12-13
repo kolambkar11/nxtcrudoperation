ba0d0f749f98815ab9aa64f87f09f7a99cd72c04
kolambkar11 <kolambkarashutosh@gmail.com>
2024-12-13 17:10:04 +0530

    final changes done

--- src/components/Myform.tsx
+++ src/components/Myform.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Input } from "./ui/input";
 import axios from "axios";
 import { toast } from "sonner";
